25ebba8313d5cb25f791cb457b9be54c26be59bb
dean131 <deanabdillah@gmail.com>
2023-09-05 06:28:25 +0700

    Change input type rental form

--- base/forms.py
+++ base/forms.py
@@ -3,6 +3,21 @@ from .models import Rental
 
 
 class RentalModelForm(forms.ModelForm):
+    start_date = forms.DateField(
+        widget=forms.TextInput(     
+            attrs={'type': 'date'} 
+        )
+    )  
+    end_date = forms.DateField(
+        widget=forms.TextInput(     
+            attrs={'type': 'date'} 
+        )
+    )  
+    check_out_date = forms.DateField(
+        widget=forms.TextInput(     
+            attrs={'type': 'date'} 
+        )
+    )  
     class Meta:
         model = Rental
         fields = '__all__'
